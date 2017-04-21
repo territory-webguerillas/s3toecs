ea29323d125788ec29fb11c03762302159ac110a
Antonio Maiolo <antonio@maiolo.net>
2017-04-21 16:10:03 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup
 
 setup(
   name = 'S3toECS', 
-  version = '0.1.3',
-  description = 'Tool that transfer Docker images from S3 to your ECS repository.',
+  version = '0.1.4',
+  description = 'Tool that transfers Docker images from S3 to your ECS repository.',
   author = 'Antonio Maiolo',
   author_email = 'maiolo.antonio@territory.de',
   license = 'MIT License',
   url = 'https://github.com/territory-webguerillas/s3toecs/',
-  download_url = 'https://github.com/territory-webguerillas/s3toecs/archive/0.1.3.tar.gz', 
+  download_url = 'https://github.com/territory-webguerillas/s3toecs/archive/0.1.4.tar.gz', 
   keywords = ['s3', 'ecs', 'aws', 's3 to ecs'],
-  entry_points = {
+  python_requires='>=3.6',
+  py_modules=['s3toecs'],
+entry_points = {
         'console_scripts': [
             's3toecs = s3toecs:main'
         ]
