0e35aa0db7fd60c91b39ff8b06f504b17fa82023
Antonio Maiolo <antonio@maiolo.net>
2017-04-21 15:47:55 +0200

    Removed long desc from setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 from setuptools import setup
-import os
-
-base_path = os.path.dirname(os.path.realpath(__file__))
-
-with open(os.path.join(base_path, 'README.md')) as f:
-    long_desc = f.read()
 
 setup(
   name = 'S3toECS', 
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Tool that transfer Docker images from S3 to your ECS repository.',
-  long_desc = long_desc,
   author = 'Antonio Maiolo',
   author_email = 'maiolo.antonio@territory.de',
   license = 'MIT License',
   url = 'https://github.com/territory-webguerillas/s3toecs/',
-  download_url = 'https://github.com/territory-webguerillas/s3toecs/archive/0.1.1.tar.gz', 
+  download_url = 'https://github.com/territory-webguerillas/s3toecs/archive/0.1.3.tar.gz', 
   keywords = ['s3', 'ecs', 'aws', 's3 to ecs'],
   entry_points = {
         'console_scripts': [
