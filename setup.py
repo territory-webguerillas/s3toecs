061388250e61240122da520ff238bc1d34bf0cfc
Antonio Maiolo <antonio@maiolo.net>
2017-04-21 15:21:36 +0200

    Added Readme and setup.cfg

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'Tool that transfer Docker images from S3 to your ECS repository.',
   author = 'Antonio Maiolo',
   author_email = 'maiolo.antonio@territory.de',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  url = 'https://github.com/territory-webguerillas/s3toecs/', # use the URL to the github repo
+  download_url = 'https://github.com/territory-webguerillas/s3toecs/archive/0.1.1.tar.gz', # I'll explain this in a second
+  keywords = ['s3', 'ecs', 'aws', 's3 to ecs'], # arbitrary keywords
   classifiers = [],
 )
